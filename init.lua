c27791bbe80683fefaedeca8982e5582ecf69cd2
Anil Kumar Singh <anil100391@gmail.com>
2026-03-16 13:08:52 +0530

    use better theme

--- init.lua
+++ init.lua
@@ -816,10 +816,19 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
+  {
+    'morhetz/gruvbox',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      -- Load the colorscheme here.
+      vim.cmd.colorscheme 'gruvbox'
+    end,
+  },
   -- Highlight todo, notes, etc in comments
   {
     'folke/todo-comments.nvim',
